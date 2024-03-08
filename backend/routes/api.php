affacff1f505e87ee38b3c4b9d4d3b8eb18598d7
valls <olavalls@gmail.com>
2024-03-08 00:23:04 -0300

    Create Laravel 10 Project with Ticket Model, Controller and API route

---
+++ backend/routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TicketController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::apiResource('tickets', TicketController::class);
+Route::post('tickets/upload', [TicketController::class, 'upload'])->name('tickets.upload');
